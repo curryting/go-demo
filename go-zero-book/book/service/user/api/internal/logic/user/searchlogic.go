3deba69d371d6421edc43e8ce10063dea3e5430f
curryting <907952235@qq.com>
2022-12-05 22:58:30 +0800

    update

--- go-zero-book/book/service/user/api/internal/logic/user/searchlogic.go
+++ go-zero-book/book/service/user/api/internal/logic/user/searchlogic.go
@@ -1,7 +1,11 @@
 package user
 
 import (
+	"book/service/user/model"
 	"context"
+	"errors"
+	"log"
+	"strings"
 
 	"book/service/user/api/internal/svc"
 	"book/service/user/api/internal/types"
@@ -23,8 +27,27 @@ func NewSearchLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SearchLogi
 	}
 }
 
-func (l *SearchLogic) Search(req *types.SearchReq) (resp *types.SearchReply, err error) {
+func (s *SearchLogic) Search(req *types.SearchReq) (resp *types.SearchReply, err error) {
 	// todo: add your logic here and delete this line
-
+	log.Println("name is ", req.Name)
+	// 验证参数
+	if len(strings.TrimSpace(req.Name)) == 0 {
+		return nil, errors.New("参数错误")
+	}
+	userInfo, err := s.svcCtx.UserModel.FindOneByNumber(s.ctx, req.Name)
+	switch err {
+	case nil:
+	case model.ErrNotFound:
+		return nil, errors.New("用户名不存在")
+	default:
+		return nil, err
+	}
+	return &types.SearchReply{
+		Id:       userInfo.Id,
+		Name:     userInfo.Name,
+		Gender:   userInfo.Gender,
+		Number:   userInfo.Number,
+		Password: userInfo.Password,
+	}, nil
 	return
 }
